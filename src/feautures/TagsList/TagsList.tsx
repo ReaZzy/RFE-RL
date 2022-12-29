3634ef269130a1a7b46e564ba8ab3544ba63f5a9
Maksym <maksym.nebela@praguelabs.com>
2022-12-29 20:48:52 +0100

    feat:🔨: add there are no tags block

--- src/feautures/TagsList/TagsList.tsx
+++ src/feautures/TagsList/TagsList.tsx
@@ -1,5 +1,5 @@
 import React, { FC, memo } from 'react';
-import { Stack, useTheme } from '@mui/material';
+import { Stack, Typography, useTheme } from '@mui/material';
 import TagsListItem from '@src/feautures/TagsList/TagListItem';
 import { TagType } from '@src/feautures/TagsList/tags.utils';
 
@@ -11,17 +11,21 @@ const TagsList: FC<TagListProps> = ({ tags }) => {
   const theme = useTheme();
   return (
     <Stack gap={theme.spacing(1)}>
-      {tags.map((tag) => (
-        /*
-         * @description (review) As we don't have any unique information in tags
-         * there are no correct ways to get keys unique without some random.
-         * that's why I use js crypto API to generate unique uuid for our tags :)
-         *
-         * Can i use (90% - 97% coverage):
-         * https://caniuse.com/?search=crypto
-         * */
-        <TagsListItem tag={tag} key={`tag-${crypto.randomUUID()}`} />
-      ))}
+      {tags.length > 0 ? (
+        tags.map((tag) => (
+          /*
+           * @description (review) As we don't have any unique information in tags
+           * there are no correct ways to get keys unique without some random.
+           * that's why I use js crypto API to generate unique uuid for our tags :)
+           *
+           * Can i use (90% - 97% coverage):
+           * https://caniuse.com/?search=crypto
+           * */
+          <TagsListItem tag={tag} key={`tag-${crypto.randomUUID()}`} />
+        ))
+      ) : (
+        <Typography align="center">There are no tags</Typography>
+      )}
     </Stack>
   );
 };
